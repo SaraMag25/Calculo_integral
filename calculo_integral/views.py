3e293cfe02c5a72fed880c0fdbd216fda91c0f9d
Sara <saramagalhaes242@gmail.com>
2025-09-13 14:34:46 -0300

    fazendo a funcao ajeita e alterando o erro do nome da funcao

--- calculo_integral/views.py
+++ calculo_integral/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import math
 
 def home(request):
     return render(request, 'calculo_integral/home.html') #ter que fazer esse html
@@ -29,7 +31,7 @@ def calcular_integral(request):
             })
     
     return render(request,'calcular_integral/home.html')
-def soma_riemann(funcao_str, a, b, n):
+def soma_rieamann(funcao_str, a, b, n):
    # Vai calcular a integral usaando método dos retângulos
    h = (b - a) / n #largura dos retangulos
    soma = 0
@@ -37,9 +39,30 @@ def soma_riemann(funcao_str, a, b, n):
    for i in range(n):
        x = a + i * h + h/2
        try:
-           y = avaliar_funcao(funcao_str, x) #fazer essa funcao
+           y = ajeita_funcao(funcao_str, x) 
            soma += y * h
        except:
            continue
-       
-    return soma
+   return soma
+
+def ajeita_funcao(funcao_str, x):
+    #para nao ter erro na hora de resolver
+    funcao = funcao_str.replace('^', '**')
+    funcao = funcao.replace('sen', 'math.sin')
+    funcao = funcao.replace('cos', 'math.cos')
+    funcao = funcao.replace('tan', 'math.tan')
+    funcao = funcao.replace('log', 'math.log')
+    funcao = funcao.replace('ln', 'math.log')
+    funcao = funcao.replace('exp', 'math.exp')
+    funcao = funcao.replace('sqrt', 'math.sqrt')
+    funcao = funcao.replace('pi', 'math.pi')
+    funcao = funcao.replace('e', 'math.e')
+
+    allowed_names = {
+        "x" : x,
+        "math": math,
+        "abs" : abs,
+        "pow" : pow
+    }
+
+    return eval(funcao, {"__builtins__": {}}, allowed_names)

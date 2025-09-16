94075f2a603bb11fdfb9e174396f78ebcd556307
Sara <saramagalhaes242@gmail.com>
2025-09-16 16:36:50 -0300

    ajeitando as urls

--- calculo_integral/urls.py
+++ calculo_integral/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('calcular/', views.calcular_integral, name='calcular_integral'),
-    path('visualizar/', views.visualizar_integral, name='visualizar_integral'),
+    path('visual/', views.visualizar_integral, name='visualizar_integral'),
 ]
